3b58e41aa32684f4af84f40df2a0895806f428d7
Austin Styer <austin.j.styer@gmail.com>
2024-10-04 18:21:10 -0600

    coverage test

--- __tests__/multiple.test.js
+++ __tests__/multiple.test.js
@@ -1,5 +1,6 @@
+// Test: multiply 2 * 3 to equal 6
 const multiply = require('../multiply.js');
-
+// Outcome: Pass
 test('multiply 2 * 3 to equal 6', () => {
     expect(multiply(2, 3)).toBe(6);
 });
--- multiply.js
+++ multiply.js
@@ -1,5 +1,6 @@
+// Multiplication function
 function multiply(a, b) {
     return a * b;
 }
-
+// Export the function
 module.exports = multiply; 
